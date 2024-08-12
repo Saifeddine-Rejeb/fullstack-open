ed9a211f5967597ebe3a643fdb821907236302f1
Saif <seifeddinerejeb.sr@gmail.com>
2024-08-12 15:50:24 +0100

    3.5-3.6: PhoneBook backend step 5-6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": "1",
@@ -49,6 +51,35 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })  
 
+const generateId = () => {
+  return String(Math.floor(Math.random() * 1000000))
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if(!body.name){
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+    if(!body.number){
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+    if(persons.find(person => person.name === body.name)){
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(person)
+    response.json(person)
+})
 
 
 
